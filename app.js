8b275f529e0c57248eb77ddd5186b16aa5cba4cd
Vikash kumar <vikash.kumar.mec20@itbhu.ac.in>
2022-01-17 12:20:12 +0530

    Level-3 Hashing Password

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-//We will use dotenv (using environmental varaibles)
-// to keep secrets safe.
-require('dotenv').config()
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5")
 
 const app = express();
 
@@ -20,13 +18,8 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-//We use process.env.DB_HOST.(..) to use our env. var from .env file
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
-
-
 
 app.get("/", function(req, res){
   res.render("home");
@@ -43,7 +36,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
 newUser.save(function(err){
@@ -58,7 +51,7 @@ newUser.save(function(err){
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundUser){
     if(err) {
