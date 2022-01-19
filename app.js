5c616487c9ba5753f92d4d7a6b46fdd8cb1063aa
Vikash kumar <vikash.kumar.mec20@itbhu.ac.in>
2022-01-19 13:34:43 +0530

    Add features of submitting a secret by user

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 const userSchema = new mongoose.Schema ({
   email: String,
   password: String,
-  googleId: String
+  googleId: String,
+  secret: String
 });
 
 //We setup userSchema to use passportLocalMongoose to use as a plugin.
@@ -97,14 +98,40 @@ app.get("/register", function(req, res){
 });
 
 app.get("/secrets", function(req, res){
-  // Here we will check if a user is logged in if it is not then
-  // we will redirect it to a login page.
+  User.find({"secret": {$ne: null}}, function(err, foundUsers){
+    if(err) {
+      console.log(err);
+    } else {
+      if (foundUsers) {
+        res.render("secrets", {usersWithSecrets: foundUsers});
+      }
+    }
+  });
+});
 
+app.get("/submit", function(req, res){
   if(req.isAuthenticated()) {
-    res.render("secrets");
+    res.render("submit");
   } else {
     res.redirect("/login");
   }
+});
+
+app.post("/submit", function(req, res){
+  const submittedSecret = req.body.secret;
+
+  User.findById(req.user.id,  function(err, foundUser){
+    if(err) {
+      console.log(err);
+    } else {
+      if(foundUser) {
+        foundUser.secret = submittedSecret;
+        foundUser.save(function(){
+          res.redirect("/secrets");
+        });
+      }
+    }
+  })
 });
 
 app.get("/logout", function(req, res){
